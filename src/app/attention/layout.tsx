7465b68babf65ab7a4367b1c8faae3d52ae91172
Santiago Sipoli <santiago.wd@gmail.com>
2023-09-17 19:39:20 -0300

    Increase bottom margin

--- src/app/attention/layout.tsx
+++ src/app/attention/layout.tsx
@@ -4,7 +4,7 @@ export default function AttentionLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<div className="w-full max-w-screen-lg min-h-[20rem] mx-auto mt-8 p-8 bg-white rounded-lg shadow-lg">
+		<div className="w-full max-w-screen-lg min-h-[20rem] mx-auto mt-8 mb-32 p-8 bg-white rounded-lg shadow-lg">
 			{children}
 		</div>
 	)
