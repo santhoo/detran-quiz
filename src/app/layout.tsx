10b740641b9918344f53d5b6948756eee7f9f9f9
Santiago Sipoli <santiago.wd@gmail.com>
2023-09-17 17:46:29 -0300

    Fix lang tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang="p-tbr">
+		<html lang="pt-br">
 			<body className={inter.className}>{children}</body>
 		</html>
 	)
