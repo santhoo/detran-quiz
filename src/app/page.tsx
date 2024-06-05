b2beb5ca3d79335f69c61269f2c3866d7ce6b88d
Santiago Sipoli <santiago.wd@gmail.com>
2024-06-04 22:33:50 -0300

    chore: Fix readme and front-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,29 @@ import Link from 'next/link'
 
 export default function Home() {
 	return (
-		<div>
-			<ul>
-				<li>
-					Teste Atenção
-					<ul>
-						<li>
-							<Link href="./attention/1">Atenção - Teste 1</Link>
-						</li>
-						<li>
-							<Link href="./attention/2">Atenção - Teste 2</Link>
-						</li>
-						<li>
-							<Link href="./attention/3">Atenção - Teste 3</Link>
-						</li>
-					</ul>
-				</li>
-			</ul>
+		<div className="w-full flex justify-center">
+			<div className="w-full max-w-sm p-6 mt-12 bg-white rounded-xl shadow-xl">
+				<h1 className="font-bold text-2xl">
+					Quiz Simulado Detran Psicotécnico
+				</h1>
+
+				<ul className="mt-6 space-y-2">
+					<li>
+						<Link href="./attention/1">Teste de Atenção 1 - Figura única</Link>
+					</li>
+					<li>
+						<Link href="./attention/2">
+							Teste de Atenção 2 - Múltiplas figuras
+						</Link>
+					</li>
+				</ul>
+
+				<hr className="my-6" />
+
+				<p className="text-sm text-zinc-400">
+					Mockup project by Santiago Sipoli
+				</p>
+			</div>
 		</div>
 	)
 }
